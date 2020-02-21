72bbf1b9c6fba642d05078ddad4095c7caddb2cd
Jobal <jbaldwije@gmail.com>
2020-02-21 00:17:26 -0600

    add index.js and add code to it

---
+++ index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+//not working for me
+$(".jumper").on("click", function( e ) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top - 150
+    }, 600);
+    
+});
