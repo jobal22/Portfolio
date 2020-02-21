25d90de366896d2e39f272adc11e826081d0d3db
Jobal <jbaldwije@gmail.com>
2020-02-21 09:31:18 -0600

    last min updates

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//This allows the menu button to be interactive
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -7,7 +8,9 @@ function myFunction() {
     }
   }
 
-//not working for me
+//This allows for the nav buttons to redirect. 
+//The -150 calculates the top of the screen into the equation
+//so that it redirects to the proper heigth.
 $(".jumper").on("click", function( e ) {
     
     e.preventDefault();
